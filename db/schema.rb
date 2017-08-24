17d293a8778f9a104bb34c45da3e6672841a696f
lucas steuperaert <lucas.steuperaert@gmail.com>
2017-08-24 19:41:28 +0200

    background card white

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170824135938) do
+ActiveRecord::Schema.define(version: 20170824124414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
