cc9fec9ffb873ed497db1a7a27f6eb2bbcd825b6
jeremy jodeau <jeremy.jodeau@gmail.com>
2017-08-28 12:50:40 +0200

    correct scearch by starts

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -14,7 +14,7 @@ class TripsController < ApplicationController
       @trips = Trip.all.map
       @nb_result = @trips.count
     end
-    @trips_day = @trips.find_all { |t|  t.ends_at >= date}.sort_by{|e| e[:starts_at]}.group_by { |t| t.starts_at.to_date }
+    @trips_day = @trips.find_all { |t|  t.starts_at >= date}.sort_by{|e| e[:starts_at]}.group_by { |t| t.starts_at.to_date }
     #map
     @tripsmap = @trips.find_all { |t| !t.from.latitude.nil?}
     @hash = Gmaps4rails.build_markers(@tripsmap) do |trip, marker|
