4bcb188eb31e840633a95df6d8ec81308e8b634b
Jan Kinsky <jan.a.kinsky@gmail.com>
2017-08-30 12:02:48 +0200

    facebook messenger notification and booking engine, with added safeguards

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -48,32 +48,32 @@ class TripsController < ApplicationController
       Location.create(address:params[:trip][:from], direction: "from", trip: @trip)
 
       # send Facebook messenger notifification
-
-     User.near(@trip.from.address, 40).each do |user|
-      if user.recipient_id? #add != current user !!!
-        Bot.deliver({
-          recipient: {
-            id: user.recipient_id
-          },
-          message:{
-            attachment:{
-              type:"template",
-              payload:{
-                template_type:"button",
-                text:"Hey #{user.first_name}! #{@trip.user.first_name} has created a #{@trip.category}-trip to #{@trip.to.address}, leaving on #{@trip.starts_at.strftime("%m/%d")} with #{@trip.nb_participant} seats available ",
-                buttons:[
-                  { type: 'web_url', title: 'More info', url: "http://www.calypso.surf#{trip_path(@trip)}" },
-                  { type: 'postback', title: "Send booking request", payload: {trip_id: @trip.id, user_id: user.id}.to_json }
-                ]
+      if ENV["FB_ACCESS_TOKEN"].present?
+        User.near(@trip.from.address, 40).each do |user|
+          if (user.recipient_id?) && (user != current_user)
+            Bot.deliver({
+              recipient: {
+                id: user.recipient_id
+              },
+              message:{
+                attachment:{
+                  type:"template",
+                  payload:{
+                    template_type:"button",
+                    text:"Hey #{user.first_name}! #{@trip.user.first_name} has created a #{@trip.category}-trip to #{@trip.to.address}, leaving on #{@trip.starts_at.strftime("%m/%d")} with #{@trip.nb_participant} seats available ",
+                    buttons:[
+                      { type: 'web_url', title: 'More info', url: "http://www.calypso.surf#{trip_path(@trip)}" },
+                      { type: 'postback', title: "Send booking request", payload: {trip_id: @trip.id, user_id: user.id}.to_json }
+                    ]
+                  }
+                }
               }
-            }
-          }
-        }, access_token: ENV['ACCESS_TOKEN'])
+            }, access_token: ENV['FB_ACCESS_TOKEN'])
+          end
+        end
       end
 
-      end
-
-      redirect_to root_path #back to show
+      redirect_to root_path #change for back to show
     else
       render :new
     end
