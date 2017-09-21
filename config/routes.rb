62adcaad4a2228a318fa17cc0ee50f34e20cc302
Yoan Dorny <y.n.dorny@gmail.com>
2017-09-21 10:40:50 +0200

    new try

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-scope '(:locale)', locale: /fr|en/ do
+
   resources :trips, only: [ :index, :show, :new, :create, :edit, :update ] do
     resources :comments, only: [:create]
     resources :participants, only: [ :create, :update ]
@@ -22,7 +22,7 @@ scope '(:locale)', locale: /fr|en/ do
     patch 'updatefbmessenger', on: :collection
     resources :reviews, only: [ :create ]
   end
-
+scope '(:locale)', locale: /fr|en/ do
   root to: 'pages#home'
 end
 end
